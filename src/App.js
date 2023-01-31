9b163ec9d77b919f1b5a064d4bf7e0f82d8c0fe2
Bailee <bailee.roby@gmail.com>
2023-01-30 22:54:03 -0800

    updating app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+
+ 
+
+  const [apple, setApple] = useState([""])
+
+  const [orange, setOrange] = useState([""])
+
+ function test() {
+  let fruit = ["🍎", "🍊", "🍎", "🍎", "🍎", "🍊", "🍎", "🍎", "🍊", "🍊"]
+
+  for (let i = 0; i < fruit.length; i++) {
+    if (fruit[i] === "🍎") {
+       setApple(prevApple => prevApple += "🍎") 
+    } else if (fruit[i] === "🍊") {
+       setOrange(prevOrange => prevOrange += "🍊")
+    }
+  }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Apples & Oranges</h1>
+        <p>🍎🍊🍎🍎🍎🍊🍎🍎🍊🍊</p>
+        <button onClick = {test} >Click Here To Sort Fruit!</button>
+        <div className = "apple-shelf">{apple}</div>  
+        <div className = "orange-shelf">{orange}</div>
     </div>
   );
 }
